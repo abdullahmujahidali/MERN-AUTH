7b4eca816f6fbafd59db32cb4142ac9477edffc3
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-01-06 16:46:52 +0500

    added router and controller for user and modified readme.md

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 const port = process.env.PORT || 5000;
 
+import userRoutes from "./routes/userRoutes.js";
+
 const app = express();
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
-});
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
 });
