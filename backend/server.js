67a9695c7ad4131e63c7628de20e68e1a6489282
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-01-06 19:32:59 +0500

    Mongodb connected and configured

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -8,8 +10,9 @@ const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
-app.use("/api/users", userRoutes);
+connectDB();
 
+app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
